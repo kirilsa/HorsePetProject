f51b567767852c6cc3dba2d34fec240cb85b326b
kirilsa <kirilsa@ukr.net>
2024-05-15 20:51:17 +0100

    Finaly

---
+++ testPetProject2/GlobalUsings.cs
@@ -0,0 +1,11 @@
+﻿global using Microsoft.AspNetCore.Authentication;
+global using Auth0.AspNetCore.Authentication;
+global using Microsoft.AspNetCore.Mvc;
+global using Microsoft.AspNetCore.Authorization;
+global using Microsoft.AspNetCore.Authentication.Cookies;
+namespace testPetProject2
+{
+    public class GlobalUsings
+    {
+    }
+}
